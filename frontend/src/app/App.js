f11552957bc7d99c7bed12229bd05721327fe8cd
tarasmyko <tarasmyko@gmail.com>
2019-03-06 11:10:16 +0100

    add: routes in projects

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as ROUTES from './const/routes';
 
 import Header from './components/header';
 import {Container} from './../styles/blocks/layout';
@@ -9,10 +11,14 @@ import Dashboard from './pages/Dashboard';
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Header />
-        <Dashboard />
-      </Container>
+      <Router>
+        <Container>
+          <Header />
+          
+          <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+
+        </Container>
+      </Router>
     );
   }
 }
