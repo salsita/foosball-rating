754507c44ea0d0118f9b36531575d7b92061521a
petrkubat <petrk@salsitasoft.com>
2019-03-08 11:12:31 +0100

    Add production DB config

--- backend/storage/db/db-config.js
+++ backend/storage/db/db-config.js
@@ -1,7 +1,16 @@
-exports.config = {
+exports.testConfig = {
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
     password: 'XXXX',
     port: 5432,
+}
+
+exports.productionConfig = {
+    user: 'rwarcmrtvowmie',
+    host: 'ec2-54-75-232-114.eu-west-1.compute.amazonaws.com',
+    database: 'd4fdcat6vlkd1k',
+    password: '94b49764bb25dc4aaa182327a4b20f8c764feaf5b9c18488b632de67b896ebd4',
+    port: 5432,
+    ssl: true
 }
