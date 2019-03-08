24bf4b099203236121271fb3e7cc8ab88b593c96
petrkubat <petrk@salsitasoft.com>
2019-03-08 10:00:17 +0100

    Remove unused import

--- frontend/src/app/components/BattleHistory/Team.js
+++ frontend/src/app/components/BattleHistory/Team.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import {
   WinnerSpan,
-  ListItem,
   ALink,
 } from '../../../styles/blocks';
-import cx from 'classnames';
 
 const createListFromTeam = (team) => team
   .map(player => <ALink>{player.name} ({player.matchRating})</ALink>)
