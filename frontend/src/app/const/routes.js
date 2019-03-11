22222ad1efdc6a1176f1ed57332a14ecfb2b86f2
petrkubat <petrk@salsitasoft.com>
2019-03-11 18:35:19 +0100

    Implement the user profile screen (and links to it)

--- frontend/src/app/const/routes.js
+++ frontend/src/app/const/routes.js
@@ -1,6 +1,6 @@
 export const DASHBOARD = '/'
 export const CREATE_MATCH = '/create-match'
-export const PROFILE = '/profile/:userId';
+export const PROFILE = '/profile/:userId'
 export const ADD_USER = '/add-user'
 export const USER_LIST = '/user-list'
 export const MATCH_LIST = '/match-list'
