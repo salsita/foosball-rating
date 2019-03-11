ad47579e12c0b077746bfe49c9c76d1eedb668c0
petrkubat <petrk@salsitasoft.com>
2019-03-11 14:07:15 +0100

    Redirect user to create match screen using Create Match button

--- frontend/src/app/const/routes.js
+++ frontend/src/app/const/routes.js
@@ -1,4 +1,4 @@
 export const DASHBOARD = '/';
-export const CREATEMATCH = '/create-match';
+export const CREATE_MATCH = '/create-match';
 export const PROFILE = '/profile';
 export const ADD_USER = '/add-user';
--- frontend/src/app/pages/Dashboard/index.js
+++ frontend/src/app/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
 import { 
   Subtitle,
   Button
@@ -7,12 +8,17 @@ import {
 import { BattleHistory } from './../../components/BattleHistory/BattleHistory';
 import { TopRating } from '../../components/TopRatings/TopRating';
 import { CreateMatchStatus } from '../../components/CreateMatch/CreateMatchStatus';
+import { CREATE_MATCH } from '../../const/routes'
 
 class DashboardComponent extends Component {
+  createMatch = () => {
+    this.props.history.push(CREATE_MATCH)
+  }
+
   render() {
     return(
       <>
-        <Button>Add Match</Button>
+        <Button onClick={this.createMatch}>Add Match</Button>
         <CreateMatchStatus status={this.props.createMatchStatus} />
         <Subtitle textAlign="center">Last Battles</Subtitle>
         <BattleHistory maxItems={5} />
@@ -27,4 +33,6 @@ const mapStateToProps = state => ({
   createMatchStatus: state.matches.status
 })
 
-export const Dashboard = connect(mapStateToProps)(DashboardComponent)
+const RoutingDashboardComponent = withRouter(DashboardComponent)
+
+export const Dashboard = connect(mapStateToProps)(RoutingDashboardComponent)
