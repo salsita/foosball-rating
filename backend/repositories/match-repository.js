e6d23f5eb09e4d325daf00d6f554ae3fee4b9428
petrkubat <petrk@salsitasoft.com>
2019-03-12 16:46:25 +0100

    Code review changes

--- backend/repositories/match-repository.js
+++ backend/repositories/match-repository.js
@@ -10,50 +10,21 @@ const updateRatingForTeam = async (team, difference) => {
     }))
 }
 
-const fillPlayersForMatch = async (match) => {
-    const filledTeam1 = await Promise.all(match.team1.map(storage.getUser))
-    const filledTeam2 = await Promise.all(match.team2.map(storage.getUser))
-
-    return {
-        ...match,
-        team1: filledTeam1,
-        team2: filledTeam2
-    }
-}
-
-const fillDateForMatch = (match) => ({ ...match, date: new Date() })
-
-const recordFilledMatch = async (filledMatch) => {
-    const winningTeam = filledMatch.team1Won ? filledMatch.team1 : filledMatch.team2
-    const losingTeam = filledMatch.team1Won ? filledMatch.team2 : filledMatch.team1
-
-    const ratingChange = ratingCalculator.computeRatingChange(winningTeam, losingTeam)
-
-    const matchWithRatingChange = {
-        ...filledMatch,
-        ratingChange
-    }
-
-    const result = await storage.insertMatch(matchWithRatingChange)
+const storeMatch = async (match) => {
+    const result = await storage.insertMatch(match)
     
-    await updateRatingForTeam(winningTeam, ratingChange)
-    await updateRatingForTeam(losingTeam, 0 - ratingChange)
+    const winningTeam = match.team1Won ? match.team1 : match.team2
+    const losingTeam = match.team1Won ? match.team2 : match.team1
+
+    await updateRatingForTeam(winningTeam, match.ratingChange)
+    await updateRatingForTeam(losingTeam, -match.ratingChange)
     
     return result
 }
 
-/**
-  * @param match Description of the match to record.
-  * @param {Array<number>} match.team1 Array of 1 or 2 elements containing IDs of players from team1.
-  * @param {Array<number>} match.team2 Array of 1 or 2 elements containing IDs of players from team2.
-  * @param {boolean} match.team1Won True if team1 won, false if team2 won.
-  */
-exports.recordMatch = async (match) => {
-    const matchWithDate = fillDateForMatch(match)
-
-    let filledMatch
+const getFilledTeam = async (playedIds) => {
     try {
-        filledMatch = await fillPlayersForMatch(matchWithDate)
+        return await Promise.all(playedIds.map(storage.getUser))
     } catch (error) {
         console.error(error)
         if (error instanceof NotFoundError) {
@@ -61,6 +32,42 @@ exports.recordMatch = async (match) => {
         }
         throw new Error("Unable to fetch players for match.")
     }
+}
 
-    return await recordFilledMatch(filledMatch)
+const getRatingChange = ({team1, team2}, team1Won) => {
+    const winningTeam = team1Won ? team1 : team2
+    const losingTeam = team1Won ? team2 : team1
+
+    return ratingChange = ratingCalculator.computeRatingChange(winningTeam, losingTeam)
+}
+
+class Match {
+    constructor({team1, team2}, team1Won, date, ratingChange) {
+        this.team1 = team1
+        this.team2 = team2
+        this.team1Won = team1Won
+        this.date = date
+        this.ratingChange = ratingChange
+    }
+}
+
+const constructMatch = async (matchDescription) => {
+    const teams = {
+        team1: await getFilledTeam(matchDescription.team1),
+        team2: await getFilledTeam(matchDescription.team2),
+    }
+    const ratingChange = getRatingChange(teams, matchDescription.team1Won)
+    const date = new Date()
+    return new Match(teams, matchDescription.team1Won, date, ratingChange)
+}
+
+/**
+  * @param matchDescription Description of the match to record.
+  * @param {Array<number>} matchDescription.team1 Array of 1 or 2 elements containing IDs of players from team1.
+  * @param {Array<number>} matchDescription.team2 Array of 1 or 2 elements containing IDs of players from team2.
+  * @param {boolean} matchDescription.team1Won True if team1 won, false if team2 won.
+  */
+exports.recordMatch = async (matchDescription) => {
+    const match = await constructMatch(matchDescription)
+    return await storeMatch(match)
 }
