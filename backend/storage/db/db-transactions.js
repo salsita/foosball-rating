8323a649b89964b8f735631f5677df5c447da686
petrkubat <petrk@salsitasoft.com>
2019-03-13 14:36:06 +0100

    Implement all the backend DB operations in context of transactions, wrap transactions in StorageContext class

---
+++ backend/storage/db/db-transactions.js
@@ -0,0 +1,62 @@
+const { Pool } = require('pg')
+const dbConfig = require('./db-config')
+
+const pool = new Pool(dbConfig.productionConfig)
+
+class Transaction {
+    constructor(client) {
+        this.client = client
+        this.active = true
+    }
+
+    async executeQuery(query, values) {
+        if (!this.active) {
+            throw new Error("Attempting to execute query on an inactive transaction")
+        }
+
+        const res = await this.client.query(query, values)
+        
+        return res.rows
+    }
+
+    async executeSingleResultQuery(query, values) {
+        const rows = await this.executeQuery(query, values)
+        if (rows.length == 0) {
+            return null
+        }
+    
+        return rows[0]
+    }
+
+    async commit() {
+        this.active = false
+        try {
+            await this.client.query('COMMIT')
+        } finally {
+            this.client.release()
+        }
+    }
+
+    async rollback() {
+        this.active = false
+        try {
+            await this.client.query('ROLLBACK')
+        } catch (error) {
+            console.error(error)
+        } finally {
+            this.client.release()
+        }
+    }
+}
+
+exports.makeTransaction = async () => {
+    const client = await pool.connect()
+    try {
+        await client.query('BEGIN')
+    } catch (error) {
+        client.release()
+        return null
+    }
+
+    return new Transaction(client)
+}
---
+++ backend/storage/storage-context.js
@@ -0,0 +1,134 @@
+const dbTransformations = require('./db/db-transformations')
+const dbQueries = require('./db/db-queries')
+const dbErrors = require('./db/db-errors')
+const { ConflictError } = require('../errors/conflict-error')
+const { InputError } = require('../errors/input-error')
+const { NotFoundError } = require('../errors/not-found-error')
+
+class StorageContext {
+    constructor(transaction) {
+        this.transaction = transaction
+    }
+
+    async getAllUsers() {
+        let rows
+        try {
+            rows = await this.transaction.executeQuery(dbQueries.selectAllusers, [])
+        } catch (error) {
+            console.error(error)
+            throw new Error("Unable to retrieve users")
+        }
+    
+        return rows.map(dbTransformations.createUserFromDbRow)
+    }
+    
+    async getUser(userId) {
+        const query = dbQueries.selectUser
+        const values = [userId]
+    
+        let row
+        try {
+            row = await this.transaction.executeSingleResultQuery(query, values)
+        } catch (error) {
+            console.error(error)
+            throw new Error("Unable to fetch user")
+        }
+    
+        if (!row) {
+            throw new NotFoundError(`User ${userId} doesn't exist`)
+        }
+    
+        return dbTransformations.createUserFromDbRow(row)
+    }
+    
+    async updateRatingForUser(userId, newRating) {
+        const query = dbQueries.updateRatingForUser
+        const values = [newRating, userId]
+    
+        let row
+        try {
+            row = await this.transaction.executeSingleResultQuery(query, values)
+        } catch (error) {
+            console.error(error)
+            throw new Error("Unable to update rating for user")
+        }
+
+        if (!row) {
+            throw new NotFoundError(`User ${userId} doesn't exist`)
+        }
+    
+        return dbTransformations.createUserFromDbRow(row)
+    }
+    
+    async insertUser(user) {
+        const query = dbQueries.insertUser
+        const values = [user.name, user.initialRating, true, user.initialRating]
+    
+        let row
+        try {
+            row = await this.transaction.executeSingleResultQuery(query, values)
+        } catch (error) {
+            console.error(error)
+            if (dbErrors.isUniqueViolation(error)) {
+                throw new ConflictError(`User ${user.name} already exists`)
+            }
+            throw new Error("Unable to add user")
+        }
+    
+        return dbTransformations.createUserFromDbRow(row)
+    }
+    
+    async insertMatch(match) {
+        const isTeamSupported = (team) => team.length >= 1 && team.length <= 2
+        if (!isTeamSupported(match.team1) || !isTeamSupported(match.team2)) {
+            throw new InputError("Inserting teams with unsupported number of players")
+        }
+    
+        const team1Player1 = match.team1[0]
+        const team1Player2 = match.team1[1] || { }
+        const team2Player1 = match.team2[0]
+        const team2Player2 = match.team2[1] || { }
+    
+        const query = dbQueries.insertMatch
+        const values = [team1Player1.id, team1Player1.rating, team1Player2.id, team1Player2.rating,
+                        team2Player1.id, team2Player1.rating, team2Player2.id, team2Player2.rating,
+                        match.date, match.ratingChange, match.team1Won]
+        
+        let row
+        try {
+            row = await this.transaction.executeSingleResultQuery(query, values)
+        } catch (error) {
+            console.error(error)
+            throw new Error("Unable to create match")
+        }
+    
+        return dbTransformations.createMatchFromDbRow(row)
+    }
+    
+    async getAllMatches() {
+        let rows
+        try {
+            rows = await this.transaction.executeQuery(dbQueries.selectAllMatches, [])
+        } catch (error) {
+            console.error(error)
+            throw new Error("Unable to retrieve matches")
+        }
+    
+        return rows.map(dbTransformations.createMatchFromDbRow)
+    }
+
+    async commit() {
+        try {
+            await this.transaction.commit()
+        } catch (error) {
+            console.error(error)
+            throw new Error("Unable to commit transaction")
+        }
+    }
+
+    async rollback() {
+        await this.transaction.rollback()
+    }
+}
+
+exports.StorageContext = StorageContext
