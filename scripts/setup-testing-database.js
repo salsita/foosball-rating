fc9cc3cb1d5b73102ba2bb521d3ba90dd561d537
tomasn <tomasnovella@gmail.com>
2020-02-11 14:18:10 +0100

    WIP

--- scripts/setup-testing-database.js
+++ scripts/setup-testing-database.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const pool = new Pool({
-    connectionString: "postgres://ttnovella:@localhost:5432/tnovella" //process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL, // "postgres://ttnovella:@localhost:5432/tnovella" //
 })
 
 const baseDirectory = './backend/db_migrations/'
