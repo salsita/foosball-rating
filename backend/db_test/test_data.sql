2144fb549d9a53d2419a59420dc23664ef968339
Tomas Novella <tomasnovella@gmail.com>
2020-02-11 16:35:40 +0100

    Setup PR builds (#79)

---
+++ backend/db_test/test_data.sql
@@ -0,0 +1,37 @@
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('12','XFilip Naumovic','1040','true','971');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('25','XKatka H.','1002','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('24','XDenisa Svobodova','945','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('20','XPavel Moravec','829','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('26','XMichal Kuric','1055','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('22','XPetr Hermansky','853','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('10','XTomy','914','true','1004');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('16','XDavid Kubon','640','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('7','XRadek','1411','true','1032');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('11','XPepa','1182','true','981');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('15','XPetr Kubat','882','true','941');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('8','XPetr Micko','990','true','1020');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('13','XMarek','925','true','970');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('23','XMichal Venos','1106','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('21','XIlde','800','true','1000');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('9','XPetr Martisek','1038','true','1012');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('5','XStego','971','true','1054');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('6','XTonda','1323','true','1052');
+INSERT INTO  "Users"("Id","Name","Rating","Active","InitialRating") VALUES ('14','XJan Bartunek','1094','true','963');
+
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('289','10','946','21','830','15','889','20','873','2019-08-26 12:07:04.933','16','true','-16');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('290','15','873','21','846','22','903','16','717','2019-08-26 15:15:08.107','14','true','-14');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('291','15','887','9','1004','23','1079','16','703','2019-08-27 12:06:12.731','14','true','-14');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('292','5','979','10','962','14','1119','22','889','2019-08-27 12:52:04.562','18','true','-18');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('293','20','857','7','1307','10','980','15','901','2019-08-27 15:11:09.533','10','true','-10');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('294','11','1061','6','1250','7','1317','10','970','2019-08-27 19:04:03.229','15','true','-15');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('295','16','689','10','955','22','871','21','860','2019-08-28 09:20:10.164','18','true','-18');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('296','13','1000','9','1018','5','997','14','1101','2019-08-28 11:31:34.329','18','true','-18');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('297','21','842','9','1036','23','1065','16','707','2019-08-29 11:14:41.588','14','true','-14');
+INSERT INTO "Matches"("Id","Team1Player1Id","Team1Player1Rating","Team1Player2Id","Team1Player2Rating","Team2Player1Id","Team2Player1Rating","Team2Player2Id","Team2Player2Rating","Date","WinningTeamRatingChange","Team1Won","LosingTeamRatingChange") VALUES ('480','21','821','14','1101','20','841','10','920','2019-11-27 14:07:08.264','12','true','-12');
+
+
+
+
+
+
+
