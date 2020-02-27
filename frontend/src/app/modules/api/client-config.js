297268109d99ce2ca5b09de1f4114deef639c5f4
Filip <filip.horky.cz@gmail.com>
2020-02-27 17:02:58 +0100

    #66 - Readme update and eslint fixes

--- frontend/src/app/modules/api/client-config.js
+++ frontend/src/app/modules/api/client-config.js
@@ -1,5 +1,5 @@
 export const config = {
-  baseURL: `${window.location.protocol}//${window.location.hostname}` + 
+  baseURL: `${window.location.protocol}//${window.location.hostname}` +
     `${process.env.REACT_APP_POST_URL_PORT}${process.env.REACT_APP_POST_URL_PATH_BASE}`,
   timeout: 5000,
 }
