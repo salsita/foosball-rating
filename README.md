ba2407c46e01abc6b203639c830cc5f8f891682d
Filip <filip.horky.cz@gmail.com>
2020-03-05 10:55:16 +0100

    #66 - Simplify API and fix typo

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Notes on running the app locally
 1) Run npm install in both backend and frontend folders.
 
 2) Copy `frontend/.env.local.example` to `frontend/.env` 
-(does not apply in case of runnig in docker)
+(does not apply in case of running in docker)
 
 3) Run npm start in backend and THEN in frontend.
--- frontend/src/app/modules/api/client-config.js
+++ frontend/src/app/modules/api/client-config.js
@@ -1,5 +1,7 @@
 export const config = {
   baseURL: `${window.location.protocol}//${window.location.hostname}` +
-    `${process.env.REACT_APP_POST_URL_PORT}${process.env.REACT_APP_POST_URL_PATH_BASE}`,
+    `${process.env.REACT_APP_POST_URL_PORT !== "" 
+        ? ':' + process.env.REACT_APP_POST_URL_PORT : ''}` +
+    `${process.env.REACT_APP_POST_URL_PATH_BASE}`,
   timeout: 5000,
 }
