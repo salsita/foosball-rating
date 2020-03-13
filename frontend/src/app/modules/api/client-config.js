eca8143fe7504de0175d2608f3b8d0f0f03bd9e7
Filip <filip.horky.cz@gmail.com>
2020-03-13 13:13:17 +0100

    #66 - Code quality fixes

--- frontend/src/app/modules/api/client-config.js
+++ frontend/src/app/modules/api/client-config.js
@@ -1,7 +1,8 @@
+const envPort = process.env.REACT_APP_POST_URL_PORT || '';
+const postUrlPort = envPort ? `:${envPort}`: ''
+
 export const config = {
-  baseURL: `${window.location.protocol}//${window.location.hostname}` +
-    `${process.env.REACT_APP_POST_URL_PORT !== "" 
-        ? ':' + process.env.REACT_APP_POST_URL_PORT : ''}` +
-    `${process.env.REACT_APP_POST_URL_PATH_BASE}`,
+  baseURL: `${window.location.protocol}//${window.location.hostname}${postUrlPort}` +
+    process.env.REACT_APP_POST_URL_PATH_BASE,
   timeout: 5000,
 }
