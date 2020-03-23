939dfd7be779b914605bc635dec38bf8587e4278
Roman Vaivod <romanv@salsitasoft.com>
2020-03-23 17:14:52 +0100

    gh-126: Add no duplicate players in matches

---
+++ backend/db_migrations/1584979214020_add-no-duplicate-players-in-matches.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate'
+
+export const shorthands: ColumnDefinitions | undefined = undefined
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.addConstraint('Matches', 'NO_DUPLICATE_PLAYERS_IN_TEAM_1', {
+    check: '"Team1Player1Id" != "Team1Player2Id"',
+  })
+  pgm.addConstraint('Matches', 'NO_DUPLICATE_PLAYERS_IN_TEAM_2', {
+    check: '"Team2Player1Id" != "Team2Player2Id"',
+  })
+  pgm.addConstraint('Matches', 'NO_DUPLICATE_PLAYERS_IN_MATCHES', {
+    check: '"Team1Player1Id" != "Team2Player1Id" AND ' +
+           '"Team1Player1Id" != "Team2Player2Id" AND ' +
+           '"Team1Player2Id" != "Team2Player1Id" AND ' +
+           '"Team1Player2Id" != "Team2Player2Id"',
+  })
+}
