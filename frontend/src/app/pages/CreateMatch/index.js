85f201dfd0598532d17c6687675469d65638fcdd
Roman Vaivod <romanv@salsitasoft.com>
2020-03-26 19:09:17 +0100

    gh-132: Add constants for min/max players num

--- frontend/src/app/pages/CreateMatch/index.js
+++ frontend/src/app/pages/CreateMatch/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { CreateMatch } from '../../components/CreateMatch/CreateMatch'
+import { MAX_PLAYERS_IN_MATCH, MIN_PLAYERS_IN_MATCH } from '../../const/constants'
 
 export const CreateMatchPage = ({ constructUrl }) =>
-  <CreateMatch constructUrl={constructUrl} maxPlayerNumber={2} minPlayerNumber={1} />
+  <CreateMatch constructUrl={constructUrl}
+    maxPlayerNumber={MAX_PLAYERS_IN_MATCH}
+    minPlayerNumber={MIN_PLAYERS_IN_MATCH} />
