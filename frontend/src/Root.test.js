54d6313f6f54d8faa41c538416f04fbf35cfaa68
Roman Vaivod <romanv@salsitasoft.com>
2020-04-01 13:23:55 +0200

    Add jest enzyme

--- frontend/src/Root.test.js
+++ frontend/src/Root.test.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Root } from './Root'
 
+import { shallow } from 'enzyme'
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Root />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('matches snapshot', () => {
+  expect(shallow(<Root/>)).toMatchSnapshot()
 })
