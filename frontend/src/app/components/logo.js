aaae6e71816cd907f04960cf4f1ffdfd3c19b0f4
Roman Vaivod <romanv@salsitasoft.com>
2020-04-17 12:16:05 +0200

    gh-147: Fix logo id for e2e

--- frontend/src/app/components/logo.js
+++ frontend/src/app/components/logo.js
@@ -3,8 +3,8 @@ import logoSmall from './../../media/logo-small.png'
 import logo from './../../media/logo.png'
 import { Link } from 'react-router-dom'
 
-export const LogoComponent = ({ className }) => (
-  <span className={className}>
+export const LogoComponent = ({ id, className }) => (
+  <span id={id} className={className}>
     <Link id='smalllogo' to={'/'}>
       <img src={logoSmall} alt="logo" />
     </Link>
