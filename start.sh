46a9a215107df2e66e839b8a3bad4f930522eb08
Jan Kotrlik <janko@salsitasoft.com>
2021-08-26 16:33:38 +0200

    Add ssl true parameter

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ PROXY_PORT=$PORT
 unset PORT
 
 cd backend
-DATABASE_URL="${DATABASE_URL}?sslmode=require" npm start &
+DATABASE_URL="${DATABASE_URL}?sslmode=require&ssl=true" npm start &
 cd ..
 
 sed "s|%PORT%|$PROXY_PORT|g" nginx/nginx-heroku.conf > /etc/nginx/conf.d/default.conf
