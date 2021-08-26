72f92da3e5350834e3cadef32a8b503a1b2d54d2
Jan Kotrlik <janko@salsitasoft.com>
2021-08-26 16:57:47 +0200

    Export DB with params early enough

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
 PROXY_PORT=$PORT
 unset PORT
+export DATABASE_URL="${DATABASE_URL}?sslmode=require&ssl=true"
 
 cd backend
-DATABASE_URL="${DATABASE_URL}?sslmode=require&ssl=true" npm start &
+npm run create-schema
+npm start &
 cd ..
 
 sed "s|%PORT%|$PROXY_PORT|g" nginx/nginx-heroku.conf > /etc/nginx/conf.d/default.conf
