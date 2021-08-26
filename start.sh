b143e43a29118483b7ab50771146ad93ff8ea37b
Jan Kotrlik <janko@salsitasoft.com>
2021-08-26 16:22:35 +0200

    Add sslmode parameter to psql conn string

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ PROXY_PORT=$PORT
 unset PORT
 
 cd backend
-npm start &
+DATABASE_URL="${DATABASE_URL}?sslmode=require" npm start &
 cd ..
 
 sed "s|%PORT%|$PROXY_PORT|g" nginx/nginx-heroku.conf > /etc/nginx/conf.d/default.conf
