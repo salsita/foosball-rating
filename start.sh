177ee959e46d512901b3585db777974a6c88ee9d
Jan Kotrlik <janko@salsitasoft.com>
2021-08-26 18:01:20 +0200

    Remove postbuild script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 PROXY_PORT=$PORT
 unset PORT
 set -x
@@ -7,6 +9,7 @@ export DATABASE_URL="${DATABASE_URL}?sslmode=require&ssl=true&rejectUnauthorized
 set +x
 
 cd backend
+npm run create-test-db || true
 npm run create-schema
 npm start &
 cd ..
