5b5bd8ca1e4dc7c92d339131c171130a34fd7e20
Roman Vaivod <romanv@salsitasoft.com>
2021-08-27 13:15:53 +0200

    Fix

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ PROXY_PORT=$PORT
 unset PORT
 
 cd backend
-NODE_TLS_REJECT_UNAUTHORIZED=0 DATABASE_URL="${DATABASE_URL}?sslmode=require" npm start &
+NODE_TLS_REJECT_UNAUTHORIZED=0 DATABASE_URL="${DATABASE_URL}?sslmode=require" npm run start:prod &
 cd ..
 
 sed "s|%PORT%|$PROXY_PORT|g" nginx/nginx-heroku.conf > /etc/nginx/conf.d/default.conf
