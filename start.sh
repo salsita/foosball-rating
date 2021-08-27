5bb54f541a81efb57b0b0a85ac02a32249b44e43
Roman Vaivod <romanv@salsitasoft.com>
2021-08-27 11:09:51 +0200

    Fix

--- start.sh
+++ start.sh
@@ -4,12 +4,9 @@ set -eu
 
 PROXY_PORT=$PORT
 unset PORT
-export NODE_TLS_REJECT_UNAUTHORIZED=0
 export DATABASE_URL="${DATABASE_URL}?sslmode=require"
 
 cd backend
-npm run create-test-db || true
-npm run create-schema || true
 npm start &
 cd ..
 
