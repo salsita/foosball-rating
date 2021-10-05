811794c43f5bbb5c14710bfb5340e84b3c914e70
Jakub Dubovsky <jakubd@salsitasoft.com>
2021-10-05 15:12:05 +0200

    fix start script

--- scripts/heroku-start.sh
+++ scripts/heroku-start.sh
@@ -4,7 +4,9 @@
 
 # TODO install concurrently separatly as in federato
 # or keep it as non dev dep in root package.json hiir?
-npm run concurrently --kill-others-on-fail -n api,nginx \
+# TODO 2 - is there better way to run it then using
+# ./node_modules/.bin ?
+./node_modules/.bin/concurrently --kill-others-on-fail -n api,nginx \
   ./scripts/heroku-start-api.sh \
   ./scripts/heroku-start-nginx.sh
 
