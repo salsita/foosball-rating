79e5b7ff4c7b1678e0617d5c4afdb29cbee184fb
Jakub Dubovsky <jakubd@salsitasoft.com>
2021-10-05 16:10:52 +0200

    run nginx in root

--- scripts/heroku-start-nginx.sh
+++ scripts/heroku-start-nginx.sh
@@ -6,11 +6,10 @@ RUNTIME_DIR=heroku-runtime
 
 rm -rf $RUNTIME_DIR
 mkdir -p $RUNTIME_DIR
-cd $RUNTIME_DIR
 
 PROXY_PORT=$PORT
 unset PORT
-sed "s|%PORT%|$PROXY_PORT|g" ../nginx/nginx-heroku.conf > nginx.conf
+sed "s|%PORT%|$PROXY_PORT|g" nginx/nginx-heroku.conf > $RUNTIME_DIR/nginx.conf
 
 mkdir -p /tmp/nginx/log
 ( cd /tmp/nginx/log && touch access.log error.log )
@@ -18,12 +17,12 @@ mkdir -p /tmp/nginx/log
 ( tail -qF -n 0 /tmp/nginx/log/*.log ) &
 
 curl -sSL https://github.com/salsita/nginx-heroku-build/releases/download/v1.0/nginx-heroku-20 \
-  > nginx
+  > $RUNTIME_DIR/nginx
 chmod +x nginx
 curl -sSL https://github.com/salsita/nginx-heroku-build/releases/download/v1.0/mime.types \
-  > mime.types
+  > $RUNTIME_DIR/mime.types
 
 # TODO now runs as root (as opposed to foosball user)
 echo Starting nginx
-./nginx -p . -c nginx.conf
+./$RUNTIME_DIR/nginx -p $RUNTIME_DIR -c $RUNTIME_DIR/nginx.conf
 
