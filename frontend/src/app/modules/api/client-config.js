5ec7904a2978cfbfb2737179c9e59a720d798a15
Jakub Dubovsky <jakubd@salsitasoft.com>
2021-10-07 11:27:13 +0200

    ts syntax

--- frontend/src/app/modules/api/client-config.js
+++ frontend/src/app/modules/api/client-config.js
@@ -3,6 +3,6 @@ const envBasePath = process.env.REACT_APP_POST_URL_PATH_BASE || ''
 const postUrlPort = envPort ? `:${envPort}` : ''
 
 export const config = {
-  baseURL: `${window.location.protocol}//${window.location.hostname}${postUrlPort}${envBasePath}`
+  baseURL: `${window.location.protocol}//${window.location.hostname}${postUrlPort}${envBasePath}`,
   timeout: 5000,
 }
