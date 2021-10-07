4ed2206a187cad2817aa8082f15511e51bcf38c8
Jakub Dubovsky <jakubd@salsitasoft.com>
2021-10-07 10:36:16 +0200

    build cleanup

--- scripts/heroku-postbuild.sh
+++ scripts/heroku-postbuild.sh
@@ -5,7 +5,15 @@
   npm ci --include=dev &&
   npm run build)
 
+fe_files=frontend-files
+rm -rf $fe_files
+mkdir -p $fe_files
+mv frontend/build/* $fe_files/
+rm -rf frontend
+
 (cd backend &&
   npm ci --include=dev &&
-  npm run create-schema)
+  npm run create-schema &&
+  rm -rf node_modules &&
+  npm ci)
 
