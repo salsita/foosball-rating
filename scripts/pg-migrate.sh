02f3c81ffb7d25f094b297092b1c44c7302ff8ea
Jakub Dubovsky <jakubd@salsitasoft.com>
2021-10-08 09:30:36 +0200

    db migrate db url check

--- scripts/pg-migrate.sh
+++ scripts/pg-migrate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if test -z "$DATABASE_URL" ; then
+  echo "Error: DATABASE_URL not set" >&2
+  exit 1
+fi
+
 if test "$DATABASE_SSL" == "true" ; then
   PGSSLMODE=require
   authRejection='--no-reject-unauthorized'
